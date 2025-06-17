38b3372d5b119fb6ba2f79ec602a9174075d6bf8
Anjali V A <anjaliva200520@gmail.com>
2025-06-17 10:15:53 +0000

    Add pre-commit hook to run tests

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,6 +1,6 @@
 const todoList = require('../todo');
 
-const {all, markAsComplete, add} =todoList();
+const {all, markAsComplete, add,overdue,dueToday,dueLater,today,tomorrow,yesterday} =todoList();
 
 
 describe("Todolist Test Suite ",() =>{
@@ -12,7 +12,8 @@ describe("Todolist Test Suite ",() =>{
                 dueDate: new Date().toISOString().slice(0,10)
             }
         );
-    })
+    });
+
     test("Should add new todo",() =>{
        const todoItemsCount =all.length;
        add(
@@ -22,12 +23,54 @@ describe("Todolist Test Suite ",() =>{
                 dueDate: new Date().toISOString().slice(0,10)
             }
         );
-        
+      
         expect(all.length).toBe(todoItemsCount + 1);
     });
+
     test("Should mark a todo as complete",() =>{
         expect(all[0].completed).toBe(false);
         markAsComplete(0);
         expect(all[0].completed).toBe(true);
-    })
+    });
+
+
+    test("checks retrieval of overdue items",() =>{
+        add(
+            {
+                title:"Test todo",
+                completed: false,
+                dueDate: yesterday
+            }
+        );
+        const overDueItems= overdue();
+        expect(overDueItems.length).not.toBe(0);
+        
+    });
+
+    test("checks retrieval of due today items",() =>{
+         add(
+            {
+                title:"Test todo",
+                completed: false,
+                dueDate: today
+            }
+        );
+        const overDueItems= dueToday();
+        expect(overDueItems.length).not.toBe(0);
+       
+    });
+
+
+    test("checks retrieval of due later items",() =>{
+        add(
+            {
+                title:"Test todo",
+                completed: false,
+                dueDate: tomorrow
+            }
+        );
+        const overDueItems= dueLater();
+        expect(overDueItems.length).not.toBe(0);
+    });
+
 });
