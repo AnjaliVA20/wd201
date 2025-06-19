25006049cb5d8fd10b49499acec11d337d69e379
Anjali V A <anjaliva200520@gmail.com>
2025-06-19 19:48:46 +0000

    Updated index.js

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,6 +1,18 @@
 const app = require("./app");
-const port = process.env.PORT || 3000;
+const db = require("./models");
 
-app.listen(port, () => {
-  console.log(`Started express server at port ${port}`);
-});
+const PORT = process.env.PORT || 3000;
+
+db.sequelize.authenticate()
+  .then(() => {
+    console.log("Connected to the database");
+    return db.sequelize.sync(); // Makes sure tables exist
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running at http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to connect to the database:", err);
+  });
